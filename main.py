2a7b43c6085af8f9bf2627e1714ce7748098546c
kalana nadun <kalananadunn99@gmail.com>
2026-08-24 01:32:27 +0530

    Hi there

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+
+with open("index.html","r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+# print(doc.prettify())
+
+tag= doc.title
+
+# print(tag)
+# print(tag.string)
